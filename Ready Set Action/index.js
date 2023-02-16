01fe94c4ea3e59edcb09b6232d07ec04457b3829
Grif Wakeman <grif@syncbeast.co>
2023-02-16 11:44:25 -0500

    Added the Ready Set Action Homework

---
+++ Ready Set Action/index.js
@@ -0,0 +1,29 @@
+function main() {
+    const growMe = document.getElementById("grow-me");
+    growMe.classList.add("big");
+
+    const shrinkMe = document.getElementById("shrink-me");
+    shrinkMe.classList.remove("big");
+
+    const lis = document.querySelectorAll("li");
+    
+    for (let li of lis) {
+        console.log(li.innerText);
+    };
+
+    const fixLink = document.querySelector("a");
+    fixLink.setAttribute("href", "https://www.example.com");
+    fixLink.innerText = "somewhere";
+
+    const hideMe = document.getElementById("hide-me");
+    hideMe.style.display = "none";
+
+    const showMe = document.getElementById("show-me");
+    showMe.style.display = "block";
+
+    const getName = document.getElementById("name").value;
+
+
+    const welcome = document.querySelector("h1");
+    welcome.innerText = (`Welcome ${getName}!`)
+}
